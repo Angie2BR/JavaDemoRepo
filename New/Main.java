9ea2cfb10ba6c79ea5f7e4f1ebef1b71d2bfa0d6
Angie2BR <abrumfield14@yahoo.com>
2023-04-22 04:08:47 -0500

    trying to add more

---
+++ New/Main.java
@@ -0,0 +1,10 @@
+package New;
+
+public class Main {
+    public static void main(String[] args) {
+       Stuff x = new Stuff("hello");
+    
+    
+}
+
+}
---
+++ New/Stuff.java
@@ -0,0 +1,11 @@
+package New;
+
+public class Stuff {
+    // add code here
+    String hello;
+
+    public Stuff(String hello){
+       System.out.println(hello);
+        
+    }
+}
